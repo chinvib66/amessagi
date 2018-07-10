06960345968f265c52a260d9ac98e5d9c7cba3f2
unknown <chinmayvib@gmail.com>
2018-07-10 19:25:06 +0530

    commit 6

--- msg/models.py
+++ msg/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Message(models.Model):
     rec_add = models.EmailField(null = False)
-    title   = models.CharField(max_length = 255, null = True, required = False)
+    title   = models.CharField(max_length = 255, null = True)
     message = models.TextField()
 
     def __str__(self):
